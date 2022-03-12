4efdeea314d467a51f7aa166e0a2b7d95236ad41
yuniskhamis <yuniskhamis3@gmail.com>
2022-03-11 23:00:38 -0500

    Created User data model to get and set name, screen name and public image url

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.kt
@@ -0,0 +1,25 @@
+package com.codepath.apps.restclienttemplate.models
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import org.json.JSONObject
+
+@Parcelize
+class User(
+           var name: String = "",
+           var screenName: String = "",
+           var publicImageUrl: String = ""
+): Parcelable {
+
+
+    companion object {
+        fun fromJson(jsonObject: JSONObject): User {
+            val user = User()
+            user.name = jsonObject.getString("name")
+            user.screenName = jsonObject.getString("screen_name")
+            user.publicImageUrl = jsonObject.getString("profile_image_url_https")
+
+            return user
+        }
+    }
+}
